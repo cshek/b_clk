0ac998f85f4e0cd992a67fbd808b6b968c59f0dc
Shek <C.K.Shek@Mattel.com>
2021-08-11 14:44:56 +0800

    Adding other files

---
+++ test.c
@@ -0,0 +1,19 @@
+// test sdcc for z80 trainer
+__sfr __at 0x40 GPIO1;
+
+void delay(int j)
+{
+	int i;
+	for(i=0; i<j; i++)
+	continue;
+}
+
+main()
+{
+	int n=0;
+	while(1)
+	{
+		GPIO1=n++;
+		delay(500);
+	}
+}
